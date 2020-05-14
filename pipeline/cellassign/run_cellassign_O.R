350c52b722909c2f2f1dab3940b2e1ffc060a518
amunzur <amunzur@krcds.0ltngohc5k0eze10ob3ctgnamd.ux.internal.cloudapp.net>
2020-05-14 15:52:45 -0700

    minor updates

--- pipeline/cellassign/run_cellassign_O.R
+++ pipeline/cellassign/run_cellassign_O.R
@@ -3,19 +3,20 @@ source(here('pipeline', 'sourceFiles', 'utilities.R'))
 source(here('pipeline', 'cellassign', 'create_marker_matrix.R'))
 source(here('pipeline', 'cellassign', 'assign_cell_type.R'))
 
-id <- 'DH13_trial'
+sample_id <- 'DH15'
 
-marker_mat <- create_marker_mat(output_file_name = paste('../data/cellassign', id, 'marker.mat.rds', sep = '/'), 
-                                mode = 'split_epithelial') 
+marker_mat <- create_marker_mat(output_file_name = paste('../data/cellassign', sample_id, 'marker.mat.rds', sep = '/'), 
+                                mode = 'split_epithelial')
+
 
 
 #### Run cell assign
-find_cell_type(path_to_sceqc = '/huntsman/amunzur/data/batch_corrected/VOA11068_ENOC-DH13-DH18/corrected.rds', 
-               path_to_marker_mat = paste('../data/cellassign', id, 'marker.mat.rds', sep = '/'),  
-               output_file_name1 = paste('../data/cellassign', id, 'sce_norm_cas.rds', sep = '/'), 
-               output_file_name2 = paste('../data/cellassign', id, 'cellassignment.rds', sep = '/'))
+find_cell_type(path_to_sceqc = '/huntsman/amunzur/data/processed/DH15/sce_with_sum_factors.rds', 
+               path_to_marker_mat = paste('../data/cellassign', sample_id, 'marker.mat.rds', sep = '/'),  
+               output_file_name1 = paste('../data/cellassign', sample_id, 'sce_pro_cas.rds', sep = '/'), 
+               output_file_name2 = paste('../data/cellassign', sample_id, 'cellassignment.rds', sep = '/'))
 
 sce <- SingleCellExperiment(list(counts = GetAssayData(seurat)))
 sce$id <- seurat$orig.ident
 
-saveRDS(sce, file = '/huntsman/amunzur/data/integrated/VOA11068_ENOC-DH13-DH18/integrated_sce.rds')
+saveRDS(sce, file = '/huntsman/amunzur/data/cellassign/DH17/sce_pro_cas.rds')
