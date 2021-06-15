e3c86b4815014ba718a105631bc258f33ac927ae
harpercheng91 <xiaolancheng.91@gmail.com>
2021-06-15 19:22:46 +0000

    change input file from sce_cas to cell type csv and sce_clus

--- pipeline/velocity/seurat_cas.R
+++ pipeline/velocity/seurat_cas.R
@@ -4,20 +4,23 @@ source(here('pipeline', 'sourceFiles', 'utilities.R'))
 
 parser <- ArgumentParser(description = "make a seurat object with cell type information")
 
-parser$add_argument('--sce_cas_path', metavar='FILE', type='character', help="Path raw sce")
+parser$add_argument('--cell_type_csv', metavar='FILE', type='character', help="Path to cell type csv file")
+parser$add_argument('--sce_clus', metavar='FILE', type='character', help='Path to sce clus rds')
 parser$add_argument('--path_to_loom_file', metavar='FILE', type='character', help="output path for loom file")
-parser$add_argument('--path_to_barcode_tsv', metavar='FILE', type='character', help="output path for tsv file containing barcode information")
 
 args <- parser$parse_args()
 
-make_seurat_object <- function(sce_cas_path, 
-                               path_to_loom_file,
-                               path_to_barcode_tsv) {
+make_seurat_object <- function(cell_type_csv, 
+                               sce_clus,
+                               path_to_loom_file) {
   
-  # read the sce with cell type info 
-  sce <- readRDS(sce_cas_path)
+  # add cell type information to sce 
+  sce <- readRDS(sce_clus)
+  cell_type <- read_csv(cell_type_csv)
+  sce$cell_type <- cell_type$cell_type
+  
+  # create seurat object
   sobject = CreateSeuratObject(counts = counts(sce))
-  
   sobject <- FindVariableFeatures(sobject)
   
   # add cell type info 
@@ -27,18 +30,19 @@ make_seurat_object <- function(sce_cas_path,
   seurat_cas.loom <- as.loom(sobject, filename = path_to_loom_file, verbose = FALSE)
   
   # save barcode in tsv
-  write.table(as.data.frame(colData(sce_pro_cas)$Barcode), 
-              file=path_to_barcode_tsv, quote=FALSE, col.names = NA)
+  #write.table(as.data.frame(colData(sce_pro_cas)$Barcode), 
+  #            file=path_to_barcode_tsv, quote=FALSE, col.names = NA)
   
 } # end of function 
 
-make_seurat_object(sce_cas_path = args$sce_cas_path, 
-                   path_to_loom_file = args$path_to_loom_file,
-                   path_to_barcode_tsv = args$path_to_barcode_tsv) # call the function 
+make_seurat_object(cell_type_csv = args$cell_type_csv, 
+                   sce_clus = args$sce_clus,
+                   path_to_loom_file = args$path_to_loom_file) 
 
 
-
-
-
-
-
+# cell_type_csv = '../data/cellassign/DH21_NEW/cell_types.csv'
+# sce_clus = '../data/clustered/sce/DH21_NEW/sce_clus.rds'
+# path_to_loom_file = '../data/loom_cas/DH21_NEW/seurat_cas.loom'
+# make_seurat_object(cell_type_csv = cell_type_csv, 
+#                    sce_clus = sce_clus,
+#                    path_to_loom_file = path_to_loom_file) 
